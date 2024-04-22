d79ef246734b12aca1ebadb511e30df6da3d0388
Saúl Camacho <electrohead1200@gmail.com>
2024-04-22 13:38:18 -0600

    Conectar Database y Google Auth, no funciona el Sign In

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
+import User from "@models/user";
+import { connectDB } from "@utils/database";
 
 const handler = NextAuth({
     providers: [
@@ -8,13 +11,45 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
 
-    async session({ session }) {
-        
-    },
-    async signIn({ profile }) {
-        
+            session.user.id = sessionUser._id.toString();
+
+            return session;
+        },
+    
+
+        // every next route is a severless route -> lambda funtion ( only opens when its called)
+        async signIn({ profile, user }) {
+            try {
+                await connectDB();
+
+                // Check if User already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+
+                // If not, create a new User
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.login.replace(" ", "").toLowerCase(),
+                        image: profile.avatar_url,
+                    });
+                }
+                return true;
+                
+            } catch (error) {
+                console.log(error);
+                return false
+            }
+        },
+        }
     }
-})
+)
 
 export { handler as GET, handler as POST };
